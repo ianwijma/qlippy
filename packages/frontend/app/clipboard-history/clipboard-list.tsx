4f13d3105699299fd0fbfe17c9ce0483fbc4d9cd
ianwijma <ian@wij.ma>
2025-02-16 13:38:27 +1100

    Initial UI changes.

--- packages/frontend/app/clipboard-history/clipboard-list.tsx
+++ packages/frontend/app/clipboard-history/clipboard-list.tsx
@@ -1,23 +1,110 @@
 import {useSettings} from "../../hooks/useSettings";
-import {ClipboardSettings} from "@qlippy/common/src/settings/clipboard.settings.types";
+import {
+    ClipboardItem,
+    ClipboardItemHash,
+    ClipboardSettings
+} from "@qlippy/common/src/settings/clipboard.settings.types";
 import {useMemo} from "react";
 
 type SearchableItem = {
-
+    hash: ClipboardItemHash,
+    searchableText: string,
 }
 
 export const ClipboardList = () => {
     const {isLoading, settings} = useSettings<ClipboardSettings>('clipboard');
+    const {clipboardItems = {}} = settings ?? {}
 
-    const searchableItems = useMemo(() => {}, [])
+    const searchableItems = useMemo<SearchableItem[]>(() => {
+        if (isLoading) return [];
+
+        const {clipboardHistory, clipboardItems} = settings;
+
+        return clipboardHistory.map((hash: ClipboardItemHash) => {
+            const item = clipboardItems[hash] ?? undefined;
+            if (item) {
+                const {type, value} = item;
+                switch (type) {
+                    case "text":
+                        return {
+                            hash,
+                            searchableText: value.toLowerCase(),
+                        }
+                    case "html":
+                    case "url":
+                    case "path":
+                        const {metadata: { text }} = item
+                        return {
+                            hash,
+                            searchableText: text.toLowerCase(),
+                        }
+                    case "image":
+                        return {
+                            hash,
+                            searchableText: `image`
+                        }
+                }
+            }
+        })
+    }, [settings])
 
     return (
-        <div>
-            List
+        <div className="h-[-webkit-fill-available] overflow-y-auto">
+            <ul>
+                {searchableItems.map(({hash}) => (
+                    <li key={hash}>
+                        <ClipboardListItem item={clipboardItems[hash]} />
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
 
-const ClipboardListItem = ({item}: {item: ClipboardSettings | undefined}) => {
+type ClipboardListItemType = {
+    item: undefined | ClipboardItem
+}
 
+const ClipboardListItem = ({item}: ClipboardListItemType) => {
+    const {type, value, metadata} = item;
+
+    switch (type) {
+        case "text":
+            return (
+                <div className="truncate">
+                    {value}
+                </div>
+            )
+        case "html":
+            const {text} = metadata
+            return (
+                <div className="truncate">
+                    {text}
+                </div>
+            )
+        case "url":
+            return (
+                <div className='truncate'>
+                    {value}
+                </div>
+            )
+        case "path":
+            return (
+                <div className='truncate'>
+                    {value}
+                </div>
+            )
+        case "image":
+            return (
+                <div className='truncate'>
+                    Image: <img className='h-9' src={value} alt='Clipboard image content'/>
+                </div>
+            )
+        default:
+            return (
+                <div className="truncate">
+                    {value}
+                </div>
+            )
+    }
 }
--- packages/frontend/app/clipboard-history/page.tsx
+++ packages/frontend/app/clipboard-history/page.tsx
@@ -6,7 +6,7 @@ import {ClipboardDetails} from "./clipboard-details";
 
 export default function ClipboardHistoryPage() {
     return (
-        <div className="bg-slate-500 h-screen p-2 draggable">
+        <div className="bg-slate-500 h-screen p-2 draggable overflow-hidden">
             <div className="flex flex-col gap-1 h-full">
                 <div className="bg-red-500 h-20 not-draggable">
                     <ClipboardQuery/>
