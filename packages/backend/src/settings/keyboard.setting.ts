f2790b5fa13dc948a585da4ad7ea5801426a2afa
ianwijma <ian@wij.ma>
2025-02-27 22:26:18 +1100

    Updated keys.

--- packages/backend/src/settings/keyboard.setting.ts
+++ packages/backend/src/settings/keyboard.setting.ts
@@ -1,13 +1,20 @@
 import {createSettings} from "./createSettings";
 import {KeyboardSettings} from "@qlippy/common/src/settings/keyboard.settings.types";
 import {isMac} from "../utils/isMac";
+import {isDev} from "../utils/isDev";
+
+const DEFAULT_CLIPBOARD_KEYS = [
+    'Control',
+    isMac() ? 'Command' : 'Super',
+    isDev() ? 'l' : 'h'
+]
 
 export const keyboardSettings = createSettings<KeyboardSettings>({
     name: 'keyboard',
     defaultSettings: {
         version: 1,
         shortcuts: {
-            [isMac() ? 'Control+Command+h' : 'Control+Super+h']: {
+            [DEFAULT_CLIPBOARD_KEYS.join('+')]: {
                 target: 'window',
                 targetId: 'clipboard-history'
             }
