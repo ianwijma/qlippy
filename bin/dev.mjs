822ece882e1e33d69fa63730c7dbe881bcb003bb
ianwijma <ian@wij.ma>
2025-03-11 23:39:34 +1100

    split qlippy from development using a seperate namespace.

--- bin/dev.mjs
+++ bin/dev.mjs
@@ -7,6 +7,22 @@ const __filename = fileURLToPath(import.meta.url); // get the resolved path to t
 const __dirname = path.dirname(__filename); // get the name of the directory
 const PROJECT_ROOT = path.resolve(__dirname, '..');
 
+const backendPackage = await fs.readJSON('packages/backend/package.json');
+const currentProductName = backendPackage.productName;
+
+backendPackage.productName = `${currentProductName}-dev`;
+
+fs.writeJson('packages/backend/package.json', backendPackage, {spaces: 2});
+
+const restore = () => {
+    backendPackage.productName = currentProductName;
+    fs.writeJson('packages/backend/package.json', backendPackage, {spaces: 2});
+}
+
+process.on("SIGINT", () => {
+    restore();
+});
+
 const {result} = concurrently(
     [
         {
